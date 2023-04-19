dc3df6efae67f73a542864abd19251e0f4167d7f
Matt Mazer <mazermatt3@gmail.com>
2023-04-19 13:12:29 +1200

    adding code safety

---
+++ dist/server.js
@@ -0,0 +1,31 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const PORT = process.env.PORT || 3001;
+app.use(cors());
+app.use(express.json());
+let totalVisits = 0;
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
+app.get("/test", (res) => {
+    res.send("Connected");
+});
+app.post("/post/active/user", (req, res) => {
+    try {
+        let data = req.body.data;
+        let ip = data.ip;
+        let time = data.time;
+        let date = data.date;
+        totalVisits++;
+        console.log(`Total site visits: ${totalVisits}`);
+        res.send(`Someone visited your site! Location:${ip} Date:${date} Time:${time} Total visits:${totalVisits}`);
+        console.log(`Someone visited your site! Location:${ip} Date:${date} Time:${time}`);
+    }
+    catch (err) {
+        res.status(500).json({ message: err.message });
+        console.log(err);
+    }
+});
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
 // require("dotenv").config();
+import express from "express";
+import { Request, Response } from "express";
+import cors from "cors";
 
-const express = require("express");
 const app = express();
-const cors = require("cors");
 const PORT = process.env.PORT || 3001;
 app.use(cors());
 app.use(express.json());
@@ -13,16 +14,16 @@ app.listen(PORT, () => {
 	console.log(`Listening on port ${PORT}`);
 });
 
-app.get("/test", (req, res) => {
+app.get("/test", (res: Response) => {
 	res.send("Connected");
 });
 
-app.post("/post/active/user", (req, res) => {
+app.post("/post/active/user", (req: Request, res: Response) => {
 	try {
-		let data = req.body.data;
-		let ip = data.ip;
-		let time = data.time;
-		let date = data.date;
+		const data = req.body.data; // need to add type
+		const ip: string = data.ip;
+		const time: string = data.time;
+		const date: string = data.date;
 
 		totalVisits++;
 
@@ -35,7 +36,7 @@ app.post("/post/active/user", (req, res) => {
 		console.log(
 			`Someone visited your site! Location:${ip} Date:${date} Time:${time}`
 		);
-	} catch (err) {
+	} catch (err: any) {
 		res.status(500).json({ message: err.message });
 		console.log(err);
 	}
