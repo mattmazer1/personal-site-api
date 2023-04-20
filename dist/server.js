a1f2460949e22256d7b14df4c7f155b2fd65eb66
Matt Mazer <mazermatt3@gmail.com>
2023-04-20 12:53:22 +1200

    wip

--- dist/server.js
+++ dist/server.js
@@ -10,23 +10,39 @@ const app = (0, express_1.default)();
 const PORT = process.env.PORT || 3001;
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
+const resObj = { status: "200" };
+const resCheck = { action: "no action" };
 let totalVisits = 0;
+let storeData = null;
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
 });
 app.get("/test", (res) => {
     res.send("Connected");
 });
-app.post("/testing", (req, res) => {
+app.post("/get/active/user", (req, res) => {
+    //change post to get?
     try {
         const data = req.body.data; // need to add type
         const ip = data.ip;
         const time = data.time;
         const date = data.date;
+        console.log(data);
+        console.log(storeData);
+        if (JSON.stringify(data) === JSON.stringify(storeData)) {
+            res.json(resCheck);
+            console.log("Already visited");
+            return;
+        }
         totalVisits++;
         console.log(`Total site visits: ${totalVisits}`);
-        res.send(`Someone visited your site! Location:${ip} Date:${date} Time:${time} Total visits:${totalVisits}`);
-        console.log(`Someone visited your site! Location:${ip} Date:${date} Time:${time}`);
+        res.status(200).json(resObj);
+        // res.json(resObj);
+        // res.send(
+        // 	`Someone visited your site! Location:${ip} Date:${date} Time:${time} Total visits:${totalVisits}`
+        // );
+        console.log(`Someone visited your site! Location:${ip} Date:${date} Time:${time} Total visits:${totalVisits}`);
+        storeData = data;
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
     }
     catch (err) {
--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,17 @@ const PORT = process.env.PORT || 3001;
 app.use(cors());
 app.use(express.json());
 
+interface UserData {
+	ip: string;
+	time: string;
+	date: string;
+}
+
+const resObj = { status: "200" };
+const resCheck = { action: "no action" };
+
 let totalVisits = 0;
+let storeData: UserData | null = null;
 
 app.listen(PORT, () => {
 	console.log(`Listening on port ${PORT}`);
@@ -18,24 +28,37 @@ app.get("/test", (res: Response) => {
 	res.send("Connected");
 });
 
-app.post("/testing", (req: Request, res: Response) => {
+app.post("/get/active/user", (req: Request, res: Response) => {
+	//change post to get?
 	try {
 		const data = req.body.data; // need to add type
 		const ip: string = data.ip;
 		const time: string = data.time;
 		const date: string = data.date;
 
+		console.log(data);
+		console.log(storeData);
+
+		if (JSON.stringify(data) === JSON.stringify(storeData)) {
+			res.status(200).json(resCheck);
+			console.log("Already visited");
+			return;
+		}
+
 		totalVisits++;
 
 		console.log(`Total site visits: ${totalVisits}`);
 
-		res.send(
-			`Someone visited your site! Location:${ip} Date:${date} Time:${time} Total visits:${totalVisits}`
-		);
+		res.status(200).json(resObj);
+		// res.json(resObj);
+		// res.send(
+		// 	`Someone visited your site! Location:${ip} Date:${date} Time:${time} Total visits:${totalVisits}`
+		// );
 
 		console.log(
-			`Someone visited your site! Location:${ip} Date:${date} Time:${time}`
+			`Someone visited your site! Location:${ip} Date:${date} Time:${time} Total visits:${totalVisits}`
 		);
+		storeData = data;
 		// eslint-disable-next-line @typescript-eslint/no-explicit-any
 	} catch (err: any) {
 		res.status(500).json({ message: err.message });
